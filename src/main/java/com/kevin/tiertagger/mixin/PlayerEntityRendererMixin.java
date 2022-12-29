d96c15deda073e59d2c259c70266d0a217c2d3d2
uku <uku3lig@gmail.com>
2022-12-29 19:17:21 +0100

    switch to ModifyArgs in mixin to avoid conflicts

--- src/main/java/com/kevin/tiertagger/mixin/PlayerEntityRendererMixin.java
+++ src/main/java/com/kevin/tiertagger/mixin/PlayerEntityRendererMixin.java
@@ -2,18 +2,16 @@ package com.kevin.tiertagger.mixin;
 
 import com.kevin.tiertagger.TierTagger;
 import net.minecraft.client.network.AbstractClientPlayerEntity;
-import net.minecraft.client.render.VertexConsumerProvider;
 import net.minecraft.client.render.entity.EntityRendererFactory;
 import net.minecraft.client.render.entity.LivingEntityRenderer;
 import net.minecraft.client.render.entity.PlayerEntityRenderer;
 import net.minecraft.client.render.entity.model.PlayerEntityModel;
-import net.minecraft.client.util.math.MatrixStack;
-import net.minecraft.entity.Entity;
 import net.minecraft.entity.player.PlayerEntity;
 import net.minecraft.text.Text;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
-import org.spongepowered.asm.mixin.injection.Redirect;
+import org.spongepowered.asm.mixin.injection.ModifyArgs;
+import org.spongepowered.asm.mixin.injection.invoke.arg.Args;
 
 
 @Mixin(PlayerEntityRenderer.class)
@@ -22,15 +20,18 @@ public abstract class PlayerEntityRendererMixin extends LivingEntityRenderer<Abs
         super(ctx, model, shadowRadius);
     }
 
-    @Redirect(
+    @ModifyArgs(
             // This specifies method
             method = "renderLabelIfPresent(Lnet/minecraft/client/network/AbstractClientPlayerEntity;Lnet/minecraft/text/Text;Lnet/minecraft/client/util/math/MatrixStack;Lnet/minecraft/client/render/VertexConsumerProvider;I)V",
             // This Specifies where
             at = @At(value = "INVOKE", target = "Lnet/minecraft/client/render/entity/LivingEntityRenderer;renderLabelIfPresent(Lnet/minecraft/entity/Entity;Lnet/minecraft/text/Text;Lnet/minecraft/client/util/math/MatrixStack;Lnet/minecraft/client/render/VertexConsumerProvider;I)V", ordinal = 1)
     )
-    public void nametagReplace(LivingEntityRenderer<AbstractClientPlayerEntity, PlayerEntityModel<AbstractClientPlayerEntity>> instance, Entity entity, Text text, MatrixStack matrixStack, VertexConsumerProvider vertexConsumerProvider, int i) {
+    public void nametagReplace(Args args) {
+        PlayerEntity entity = args.get(0);
+        Text text = args.get(1);
+
         // Sorta like a tunnel: text -> appendTier(text) -> text <tier> -> super.renderLabelIfPresent(text) -> completed!
-        text = TierTagger.appendTier((PlayerEntity) entity, text);
-        super.renderLabelIfPresent((AbstractClientPlayerEntity) entity, text, matrixStack, vertexConsumerProvider, i);
+        text = TierTagger.appendTier(entity, text);
+        args.set(1, text);
     }
 }

d7060ae019eb418837262cb1da8bc8df50ef6b50
uku <uku3lig@gmail.com>
2023-12-30 19:09:29 +0100

    add the ability to change the api url

--- src/main/java/com/kevin/tiertagger/model/PlayerInfo.java
+++ src/main/java/com/kevin/tiertagger/model/PlayerInfo.java
@@ -1,6 +1,7 @@
 package com.kevin.tiertagger.model;
 
 import com.google.gson.Gson;
+import com.kevin.tiertagger.TierTagger;
 
 import java.net.URI;
 import java.net.http.HttpClient;
@@ -14,11 +15,9 @@ public record PlayerInfo(Map<String, Ranking> rankings) {
     public record Ranking(int tier, int pos) {
     }
 
-    private static final String ENDPOINT = "https://mctiers.com/api/profile/%s";
-
     public static CompletableFuture<PlayerInfo> get(HttpClient client, UUID uuid) {
-        URI formattedEndpoint = URI.create(ENDPOINT.formatted(uuidStr(uuid)));
-        final HttpRequest request = HttpRequest.newBuilder(formattedEndpoint).GET().build();
+        String endpoint = TierTagger.getManager().getConfig().getApiUrl() + "/profile/" + uuidStr(uuid);
+        final HttpRequest request = HttpRequest.newBuilder(URI.create(endpoint)).GET().build();
 
         return client.sendAsync(request, HttpResponse.BodyHandlers.ofString())
                 .thenApply(HttpResponse::body)

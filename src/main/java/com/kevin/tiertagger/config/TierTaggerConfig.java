2c085347fa24e99b69a3696ad889814aaba07f22
uku <uku3lig@gmail.com>
2023-12-30 19:09:29 +0100

    add the ability to change the api url

--- src/main/java/com/kevin/tiertagger/config/TierTaggerConfig.java
+++ src/main/java/com/kevin/tiertagger/config/TierTaggerConfig.java
@@ -11,6 +11,7 @@ import net.uku3lig.ukulib.config.IConfig;
 @NoArgsConstructor
 @AllArgsConstructor
 public class TierTaggerConfig implements IConfig<TierTaggerConfig> {
+    private String apiUrl = "https://mctiers.com/api";
     private boolean enabled = true;
     private GameMode gameMode = GameMode.VANILLA;
     private boolean showUnranked = false;

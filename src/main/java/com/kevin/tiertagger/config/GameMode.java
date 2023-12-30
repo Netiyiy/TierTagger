74a9e67a0c9fac6f6f7b298c9697d0f674d1d591
uku <uku3lig@gmail.com>
2023-12-30 19:05:12 +0100

    dynamically fetch player rankings
    fixes #13

--- src/main/java/com/kevin/tiertagger/config/GameMode.java
+++ src/main/java/com/kevin/tiertagger/config/GameMode.java
@@ -7,15 +7,16 @@ import net.minecraft.util.TranslatableOption;
 @AllArgsConstructor
 @Getter
 public enum GameMode implements TranslatableOption {
-    SWORD(0, "Sword"),
-    VANILLA(1, "Vanilla"),
-    AXE(2, "Axe"),
-    POT(3, "Pot"),
-    NETH_POT(4, "NethPot"),
-    UHC(5, "UHC"),
-    SMP(6, "SMP"),
+    SWORD(0, "Sword", "sword"),
+    VANILLA(1, "Vanilla", "vanilla"),
+    AXE(2, "Axe", "axe"),
+    POT(3, "Pot", "pot"),
+    NETH_POT(4, "NethPot", "neth_pot"),
+    UHC(5, "UHC", "uhc"),
+    SMP(6, "SMP", "smp"),
     ;
 
     private final int id;
     private final String translationKey;
+    private final String apiKey;
 }

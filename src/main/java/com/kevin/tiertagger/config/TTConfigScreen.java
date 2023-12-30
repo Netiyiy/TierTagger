82ab337eee5050340ef900167ab50f915a3461aa
uku <uku3lig@gmail.com>
2023-12-30 19:05:12 +0100

    dynamically fetch player rankings
    fixes #13

--- src/main/java/com/kevin/tiertagger/config/TTConfigScreen.java
+++ src/main/java/com/kevin/tiertagger/config/TTConfigScreen.java
@@ -5,6 +5,7 @@ import com.kevin.tiertagger.tierlist.PlayerSearchScreen;
 import net.minecraft.client.gui.screen.Screen;
 import net.uku3lig.ukulib.config.option.CyclingOption;
 import net.uku3lig.ukulib.config.option.ScreenOpenButton;
+import net.uku3lig.ukulib.config.option.SimpleButton;
 import net.uku3lig.ukulib.config.option.WidgetCreator;
 import net.uku3lig.ukulib.config.screen.AbstractConfigScreen;
 
@@ -19,13 +20,8 @@ public class TTConfigScreen extends AbstractConfigScreen<TierTaggerConfig> {
                 CyclingOption.ofBoolean("tiertagger.config.enabled", config.isEnabled(), config::setEnabled),
                 CyclingOption.ofTranslatableEnum("tiertagger.config.gamemode", GameMode.class, config.getGameMode(), config::setGameMode),
                 CyclingOption.ofBoolean("tiertagger.config.unranked", config.isShowUnranked(), config::setShowUnranked),
+                new SimpleButton("tiertagger.clear", b -> TierTagger.clearCache()),
                 new ScreenOpenButton("tiertagger.config.search", PlayerSearchScreen::new)
         };
-    }
-
-    @Override
-    public void removed() {
-        super.removed();
-        TierTagger.reloadTiers();
     }
 }
--- src/main/java/com/kevin/tiertagger/mixin/PlayerEntityRendererMixin.java
+++ src/main/java/com/kevin/tiertagger/mixin/PlayerEntityRendererMixin.java
@@ -1,11 +1,7 @@
 package com.kevin.tiertagger.mixin;
 
 import com.kevin.tiertagger.TierTagger;
-import net.minecraft.client.network.AbstractClientPlayerEntity;
-import net.minecraft.client.render.entity.EntityRendererFactory;
-import net.minecraft.client.render.entity.LivingEntityRenderer;
 import net.minecraft.client.render.entity.PlayerEntityRenderer;
-import net.minecraft.client.render.entity.model.PlayerEntityModel;
 import net.minecraft.entity.player.PlayerEntity;
 import net.minecraft.text.Text;
 import org.spongepowered.asm.mixin.Mixin;
@@ -15,23 +11,16 @@ import org.spongepowered.asm.mixin.injection.invoke.arg.Args;
 
 
 @Mixin(PlayerEntityRenderer.class)
-public abstract class PlayerEntityRendererMixin extends LivingEntityRenderer<AbstractClientPlayerEntity, PlayerEntityModel<AbstractClientPlayerEntity>> {
-    protected PlayerEntityRendererMixin(EntityRendererFactory.Context ctx, PlayerEntityModel<AbstractClientPlayerEntity> model, float shadowRadius) {
-        super(ctx, model, shadowRadius);
-    }
-
+public class PlayerEntityRendererMixin {
     @ModifyArgs(
-            // This specifies method
             method = "renderLabelIfPresent(Lnet/minecraft/client/network/AbstractClientPlayerEntity;Lnet/minecraft/text/Text;Lnet/minecraft/client/util/math/MatrixStack;Lnet/minecraft/client/render/VertexConsumerProvider;I)V",
-            // This Specifies where
             at = @At(value = "INVOKE", target = "Lnet/minecraft/client/render/entity/LivingEntityRenderer;renderLabelIfPresent(Lnet/minecraft/entity/Entity;Lnet/minecraft/text/Text;Lnet/minecraft/client/util/math/MatrixStack;Lnet/minecraft/client/render/VertexConsumerProvider;I)V", ordinal = 1)
     )
-    public void nametagReplace(Args args) {
+    public void addTierToNametag(Args args) {
         if (!TierTagger.getManager().getConfig().isEnabled()) return;
 
         PlayerEntity entity = args.get(0);
         Text text = args.get(1);
-        // Sorta like a tunnel: text -> appendTier(text) -> text <tier> -> super.renderLabelIfPresent(text) -> completed!
         text = TierTagger.appendTier(entity, text);
         args.set(1, text);
     }

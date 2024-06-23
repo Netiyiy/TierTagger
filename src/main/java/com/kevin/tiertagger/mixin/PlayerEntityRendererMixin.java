255eec4a4eb09c927ca8b84fc4b3d3d994010f8f
uku <hi@uku.moe>
2024-06-23 13:06:09 +0200

    chore: small refactor

--- src/main/java/com/kevin/tiertagger/mixin/PlayerEntityRendererMixin.java
+++ src/main/java/com/kevin/tiertagger/mixin/PlayerEntityRendererMixin.java
@@ -1,27 +1,29 @@
 package com.kevin.tiertagger.mixin;
 
 import com.kevin.tiertagger.TierTagger;
+import com.llamalad7.mixinextras.injector.wrapoperation.Operation;
+import com.llamalad7.mixinextras.injector.wrapoperation.WrapOperation;
+import net.minecraft.client.render.VertexConsumerProvider;
 import net.minecraft.client.render.entity.PlayerEntityRenderer;
+import net.minecraft.client.util.math.MatrixStack;
+import net.minecraft.entity.Entity;
 import net.minecraft.entity.player.PlayerEntity;
 import net.minecraft.text.Text;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
-import org.spongepowered.asm.mixin.injection.ModifyArgs;
-import org.spongepowered.asm.mixin.injection.invoke.arg.Args;
 
 
 @Mixin(PlayerEntityRenderer.class)
 public class PlayerEntityRendererMixin {
-    @ModifyArgs(
+    @WrapOperation(
             method = "renderLabelIfPresent(Lnet/minecraft/client/network/AbstractClientPlayerEntity;Lnet/minecraft/text/Text;Lnet/minecraft/client/util/math/MatrixStack;Lnet/minecraft/client/render/VertexConsumerProvider;IF)V",
             at = @At(value = "INVOKE", target = "Lnet/minecraft/client/render/entity/LivingEntityRenderer;renderLabelIfPresent(Lnet/minecraft/entity/Entity;Lnet/minecraft/text/Text;Lnet/minecraft/client/util/math/MatrixStack;Lnet/minecraft/client/render/VertexConsumerProvider;IF)V", ordinal = 1)
     )
-    public void addTierToNametag(Args args) {
-        if (!TierTagger.getManager().getConfig().isEnabled()) return;
+    public void addTierToNametag(PlayerEntityRenderer instance, Entity entity, Text text, MatrixStack matrixStack, VertexConsumerProvider vertexConsumerProvider, int i, float v, Operation<Void> original) {
+        if (TierTagger.getManager().getConfig().isEnabled() && entity instanceof PlayerEntity player) {
+            text = TierTagger.appendTier(player, text);
+        }
 
-        PlayerEntity entity = args.get(0);
-        Text text = args.get(1);
-        text = TierTagger.appendTier(entity, text);
-        args.set(1, text);
+        original.call(instance, entity, text, matrixStack, vertexConsumerProvider, i, v);
     }
 }

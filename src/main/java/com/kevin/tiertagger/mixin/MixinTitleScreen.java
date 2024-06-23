80d72145b82c64092ce993881a8e24adf800a5c8
uku <hi@uku.moe>
2024-06-23 12:35:47 +0200

    feat: add update checking system

---
+++ src/main/java/com/kevin/tiertagger/mixin/MixinTitleScreen.java
@@ -0,0 +1,54 @@
+package com.kevin.tiertagger.mixin;
+
+import com.kevin.tiertagger.TierTagger;
+import net.fabricmc.loader.api.FabricLoader;
+import net.fabricmc.loader.api.Version;
+import net.minecraft.client.MinecraftClient;
+import net.minecraft.client.gui.screen.ConfirmScreen;
+import net.minecraft.client.gui.screen.Screen;
+import net.minecraft.client.gui.screen.TitleScreen;
+import net.minecraft.text.Text;
+import net.minecraft.util.Formatting;
+import net.minecraft.util.Util;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.Unique;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
+
+@Mixin(TitleScreen.class)
+public class MixinTitleScreen extends Screen {
+    @Unique
+    private static boolean hasCheckedVersion = false;
+
+    protected MixinTitleScreen(Text title) {
+        super(title);
+    }
+
+    @Inject(method = "init", at = @At("RETURN"))
+    public void showUpdateScreen(CallbackInfo ci) {
+        if (hasCheckedVersion) return;
+
+        Version currentVersion = FabricLoader.getInstance().getModContainer("tier-tagger").map(m -> m.getMetadata().getVersion()).orElse(null);
+        if (currentVersion != null && currentVersion.compareTo(TierTagger.getLatestVersion()) < 0) {
+            Text newVersion = Text.literal(TierTagger.getLatestVersion().getFriendlyString()).formatted(Formatting.GREEN);
+
+            MinecraftClient.getInstance().setScreen(new ConfirmScreen(
+                    b -> {
+                        if (b) {
+                            String url = "https://modrinth.com/mod/tiertagger/version/" + TierTagger.getLatestVersion().getFriendlyString();
+                            Util.getOperatingSystem().open(url);
+                        }
+
+                        MinecraftClient.getInstance().setScreen(this);
+                    },
+                    Text.translatable("tiertagger.outdated.title"),
+                    Text.translatable("tiertagger.outdated.desc", newVersion),
+                    Text.translatable("tiertagger.outdated.download"),
+                    Text.translatable("tiertagger.outdated.ignore")
+            ));
+        }
+
+        hasCheckedVersion = true;
+    }
+}

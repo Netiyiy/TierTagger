54795e7b10a4266429775660a02dfbcb6d2f5400
uku <hi@uku.moe>
2024-08-12 12:28:46 +0200

    chore: remove unused show unranked setting

--- src/main/java/com/kevin/tiertagger/config/TTConfigScreen.java
+++ src/main/java/com/kevin/tiertagger/config/TTConfigScreen.java
@@ -21,7 +21,6 @@ public class TTConfigScreen extends AbstractConfigScreen<TierTaggerConfig> {
         return new WidgetCreator[] {
                 CyclingOption.ofBoolean("tiertagger.config.enabled", config.isEnabled(), config::setEnabled),
                 CyclingOption.ofTranslatableEnum("tiertagger.config.gamemode", GameMode.class, config.getGameMode(), config::setGameMode),
-                CyclingOption.ofBoolean("tiertagger.config.unranked", config.isShowUnranked(), config::setShowUnranked),
                 CyclingOption.ofBoolean("tiertagger.config.retired", config.isShowRetired(), config::setShowRetired),
                 CyclingOption.ofTranslatableEnum("tiertagger.config.statistic", TierTaggerConfig.Statistic.class, config.getShownStatistic(), config::setShownStatistic),
                 new SimpleButton("tiertagger.clear", b -> TierCache.clearCache()),

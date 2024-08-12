f1521ecc6a52a162224f48dd29243b4aebe5f22e
uku <hi@uku.moe>
2024-08-12 18:08:50 +0200

    feat: show the highest tier if none found

--- src/main/java/com/kevin/tiertagger/config/TTConfigScreen.java
+++ src/main/java/com/kevin/tiertagger/config/TTConfigScreen.java
@@ -5,6 +5,8 @@ import com.kevin.tiertagger.TierTagger;
 import com.kevin.tiertagger.model.GameMode;
 import com.kevin.tiertagger.tierlist.PlayerSearchScreen;
 import net.minecraft.client.gui.screen.Screen;
+import net.minecraft.client.option.SimpleOption;
+import net.minecraft.text.Text;
 import net.uku3lig.ukulib.config.option.CyclingOption;
 import net.uku3lig.ukulib.config.option.ScreenOpenButton;
 import net.uku3lig.ukulib.config.option.SimpleButton;
@@ -22,6 +24,7 @@ public class TTConfigScreen extends AbstractConfigScreen<TierTaggerConfig> {
                 CyclingOption.ofBoolean("tiertagger.config.enabled", config.isEnabled(), config::setEnabled),
                 CyclingOption.ofTranslatableEnum("tiertagger.config.gamemode", GameMode.class, config.getGameMode(), config::setGameMode),
                 CyclingOption.ofBoolean("tiertagger.config.retired", config.isShowRetired(), config::setShowRetired),
+                CyclingOption.ofBoolean("tiertagger.config.highest", config.isShowHighest(), config::setShowHighest, SimpleOption.constantTooltip(Text.translatable("tiertagger.config.highest.desc"))),
                 CyclingOption.ofTranslatableEnum("tiertagger.config.statistic", TierTaggerConfig.Statistic.class, config.getShownStatistic(), config::setShownStatistic),
                 new SimpleButton("tiertagger.clear", b -> TierCache.clearCache()),
                 new ScreenOpenButton("tiertagger.config.search", PlayerSearchScreen::new)
